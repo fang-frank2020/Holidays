0c70165694d4d7b0383a0613f520302aa969a6fa
fang-frank2020 <frankfangfrank@gmail.com>
2022-08-25 18:07:05 -0400

    first draft

--- script.js
+++ script.js
@@ -12,19 +12,27 @@
 
 
 async function getData(country, api) {
+    //fetches calendar names of given country
     const response = await fetch(api);
     const data = await response.json();
+
+    //gets today's today
     var today = new Date();
     var day = today.getDate();
     var month = today.getMonth() + 1; 
     var year = today.getFullYear();
     today = month + '/' + day + '/' + year;
-    rep = data["items"].length;
+
+    //create empty dictionaries to match holiday date with description and name
     var wikidict = {};
     var namedict = {};
     var imagedict = {};
-    dateArray = [];
+    var dateArray = [];
+    rep = data["items"].length;
+
+    //filters through holidays that are only in the future and gets descriptions and images for each holiday
     for (let i = 0; i < rep; i++) {
+        //finds holiday date
         HolDate = data["items"][i]["start"]["date"];
 
         HolYear = HolDate.slice(0,4);
@@ -36,6 +44,7 @@ async function getData(country, api) {
         HolDay = HolDate.slice(8, 10);
         HolDay = parseInt(HolDay);
         
+        //gets rid of unnecessary words in holiday name
         var name = data["items"][i]["summary"];
         if ((HolYear > year ||  (HolYear == year && HolMonth > month) || (HolYear == year && HolMonth == month && HolDay > day)) && name.includes("substitute") == false) {
             dateArray.push(HolDate);
@@ -54,18 +63,17 @@ async function getData(country, api) {
             //const wikilink = await fetch ("https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=" + name.replaceAll(' ', "_") + "&limit=1&namespace=0&format=json");
             //const wikidata = await wikilink.json();
             
-
+            //gets summary of holiday
             const wikisumdata = await fetch ("https://en.wikipedia.org/api/rest_v1/page/summary/" + name.replaceAll(' ', "_"));
             const wikisum = await wikisumdata.json();
-
-            const wikiImageData = await fetch ("https://en.wikipedia.org/w/api.php?origin=*&action=query&formatversion=2&prop=pageimages%7Cpageterms&format=json&titles=" + name.replaceAll(' ', "_"));
-            const wikiImage = await wikiImageData.json();
-
             var summary = wikisum["extract"];
             if (summary == undefined) {
                 summary = "Sorry, I couldn't find information about this holiday on Wikipedia.";
             }
 
+            //gets image of holiday
+            const wikiImageData = await fetch ("https://en.wikipedia.org/w/api.php?origin=*&action=query&formatversion=2&prop=pageimages%7Cpageterms&format=json&titles=" + name.replaceAll(' ', "_"));
+            const wikiImage = await wikiImageData.json();
             var imagesource = wikiImage["query"]["pages"][0];
             if (imagesource["thumbnail"] == undefined || wikiImage["query"]["pages"][0]["thumbnail"]["source"] == undefined) {
                 var image = "Sorry, I couldn't find the image for this holiday on Wikipedia.";
@@ -74,6 +82,8 @@ async function getData(country, api) {
                 var image = wikiImage["query"]["pages"][0]["thumbnail"]["source"];
             }
             console.log(image);
+
+            //sets date to image, summary, and name
             imagedict[HolDate] = image;
             wikidict[HolDate] = summary;
             namedict[HolDate] = name;
@@ -89,6 +99,7 @@ async function getData(country, api) {
             //console.log(newslink);
         }
     }
+    //sorts holiday dates and finds repeated dates
     var repeatdict = {};
     var arraySorted = dateArray.sort(function(a,b){
         if (new Date(a) - new Date(b) == 0) {
@@ -117,6 +128,7 @@ async function getData(country, api) {
     
     var repeatKeys = Object.keys(repeatdict);
 
+    //finds how many repeated dates there are
     for (let i = 0; i < repeatKeys.length; i++) {
         if (repeatdict[repeatKeys[i]] != 1) {
             var counter = 0;
@@ -129,15 +141,18 @@ async function getData(country, api) {
         }
     }
     
+    //sets sorted dates to respective name, summary, and image
     var sortedWiki = {};
     var sortedName = {};
-    var sortedImage = {}
+    var sortedImage = {};
     for (let i = 0; i < arraySorted.length; i++) {
         sortedWiki[arraySorted[i]] = wikidict[arraySorted[i]];
         sortedName[arraySorted[i]] = namedict[arraySorted[i]];
         sortedImage[arraySorted[i]] = imagedict[arraySorted[i]];
     }
     console.log(sortedImage);
+
+    //sorts dates of repeated dates that are missing in the main set
     var sortedMissing = {};
     var sortedKeys = repeatKeys.sort(function(a,b){
         return new Date(a) - new Date(b);
@@ -146,7 +161,10 @@ async function getData(country, api) {
         sortedMissing[sortedKeys[i]] = repeatdict[sortedKeys[i]];
     }
 
+    //displays the holidays with their respective information(name, date, summary, image)
     for (let i = 0; i < Object.keys(sortedWiki).length; i++) {
+        
+        //displays name
         var blockdiv = document.createElement("div");
         blockdiv.id = "block";
         document.getElementsByTagName('body')[0].appendChild(blockdiv);
@@ -156,6 +174,7 @@ async function getData(country, api) {
         container.id = "container";
         blockdiv.appendChild(container);
 
+        //displays image
         if (Object.values(sortedImage)[i].includes("https") == true) {
             var firstinner = document.createElement("img");
             container.appendChild(firstinner);
@@ -169,12 +188,14 @@ async function getData(country, api) {
             noimg.innerHTML = Object.values(sortedImage)[i];
         }
 
+        //displays summary
         var secondinner = document.createElement("div");
         secondinner.id = "summary";
         container.appendChild(secondinner);
         secondinner.innerHTML = Object.values(sortedWiki)[i];
     }
 
+    //displays the repeated holidays that are missing in the main set
     for (let i = 0; i < repeatKeys.length; i++) {
         var missingdiv = document.createElement("div");
         missingdiv.id = "missing";
@@ -188,6 +209,7 @@ async function getData(country, api) {
     }
 }
 
+//gets the holidays with their name and dates for a given country
 function getAPI(countryname) {
     return "https://www.googleapis.com/calendar/v3/calendars/en."+ countryname +"%23holiday%40group.v.calendar.google.com/events?key=AIzaSyBOS4bvkIVhWtcCb6L2dz0Q1CeJsl8ZwU4";
 }
